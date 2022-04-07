44b07879fc4b3ad14a87eda85a3685ccfbc983fb
omeralii <78183586+omeralii@users.noreply.github.com>
2022-04-06 21:03:35 -0400

    initial commit

---
+++ website_app/src/component/Basics/MenuApi.js
@@ -0,0 +1,86 @@
+const Menu = [
+        {
+          id: 1,
+          image: "images/waffles.jpg",
+          name: "Belgian Waffles",
+          category: "breakfast",
+          price: "16",
+          description:
+            "Belgian waffles are a variety of waffle with a lighter batter, larger squares, and deeper pockets than American waffles. Belgian waffles were originally leavened with yeast, but baking powder is now often used.",
+        },
+      
+        {
+          id: 2,
+          image: "images/burger.jpg",
+          name: "Beef Burger",
+          category: "evening",
+          price: "10",
+          description:
+            "A thick, juicy beef patty, cheese losing its hard edges as it melts overtop, a slathering of sauce, a spoonful of tangy relish and a couple of slices of smoky, salty bacon, a bit of onion, some tomato and crisp lettuce all tucked between two halves of a bun: it is all the things I could want. ",
+        },
+        {
+          id: 3,
+          image: "images/frenchtoast.jpg",
+          name: "French Toast",
+          category: "breakfast",
+          price: "10",
+          description:
+            "French toast is a dish made of sliced bread soaked in beaten eggs and typically milk, then pan fried. Alternative names and variants include 'eggy bread', 'Bombay toast', 'gypsy toast', and 'poor knights'.",
+        },
+        {
+          id: 4,
+          image: "../images/bowl.jpg",
+          name: "Chicken Bowl",
+          category: "lunch",
+          price: "40",
+          description:
+            "Chicken salad is any salad with chicken as a main ingredient. Other common ingredients may include mayonnaise, hard-boiled egg, celery, onion, pepper, pickles and a variety of mustards.",
+        },
+        {
+          id: 5,
+          image: "../images/pizza.jpg",
+          name: "pizza",
+          category: "evening",
+          price: "70",
+          description:
+            "Pizza is a dish of Italian origin consisting of a usually round, flat base of leavened wheat-based dough topped with tomatoes, cheese, and often various other ingredients (such as anchovies, mushrooms, onions, olives, pineapple, meat, etc.)",
+        },
+        {
+          id: 6,
+          image: "../images/steak.jpg",
+          name: "Fillet Mignon",
+          category: "dinner",
+          price: "180",
+          description:
+            "Filet mignon is a cut of meat taken from the smaller end of the tenderloin, or psoas major of an animal carcass. In French, it mostly refers to cuts of pork tenderloin. ",
+        },
+        {
+          id: 7,
+          image: "../images/pasta.jpg",
+          name: "Alfredo Pasta",
+          category: "dinner",
+          price: "90",
+          description:
+            "In 1914, Alfredo di Lelio, a Roman man with a Restaurant in (where grandma was born and raised) was popular among American tourists, and he named his butter and cheese linguine after himself. ",
+        },
+        {
+          id: 8,
+          image: "../images/sandwich.jpg",
+          name: "Club Sandwich",
+          category: "lunch",
+          price: "35",
+          description:
+            "In 1914, Alfredo di Lelio, a Roman man with a Restaurant in (where grandma was born and raised) was popular among American tourists, and he named his butter and cheese linguine after himself.",
+        },
+        {
+          id: 9,
+          image: "../images/rolls.jpg",
+          name: "Spring Rolls",
+          category: "evening",
+          price: "20",
+          description:
+            "Spring rolls are a large variety of filled, rolled appetizers or dim sum found in East Asian, Southeast Asian cuisine.  ",
+        },
+      ];
+
+export default Menu;
